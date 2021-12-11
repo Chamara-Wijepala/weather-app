dac61814cf50d1288cc186e7c19fa11e867962f6
Chamara-Wijepala <mr.worf777@gmail.com>
2021-12-11 18:17:27 +0530

    Add functions to render API data to the DOM

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import './index.css';
 import getData from './modules/fetch-data';
+import setInnerHtml from './modules/dom-functions';
+
+const
+locationName = document.getElementById('location-name'),
+weatherDescription = document.getElementById('weather-description'),
+weatherIcon = document.getElementById('weather-icon'),
+temperature = document.getElementById('temperature'),
+feelsLike = document.getElementById('feels-like'),
+wind = document.getElementById('wind'),
+humidity = document.getElementById('humidity');
 
 getData()
     .then(response => {
-        console.log(response)
-    })
+        setInnerHtml(locationName, response.name);
+        setInnerHtml(weatherDescription, response.weather[0].description);
+        setInnerHtml(weatherIcon, response.weather[0].icon);
+        setInnerHtml(temperature, response.main.temp);
+        setInnerHtml(feelsLike, response.main.feels_like);
+        setInnerHtml(wind, response.wind.speed);
+        setInnerHtml(humidity, response.main.humidity);
+    });
---
+++ src/modules/dom-functions.js
@@ -0,0 +1,3 @@
+export default function setInnerHtml(element, html) {
+    element.innerHTML = html;
+};
